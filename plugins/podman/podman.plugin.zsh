e3806c99c68e50f2d42a537097e2b2dc427a01e8
Yudha Setiawan <yudha.setiawan@koinworks.com>
2023-12-29 13:44:34 +0700

    feat(podman): update alias

--- plugins/podman/podman.plugin.zsh
+++ plugins/podman/podman.plugin.zsh
@@ -8,10 +8,22 @@ if [[ $PMSPEC != *b* ]] {
   PATH="${0:h}/bin:${PATH}"
 }
 
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it to `podman`. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_podman" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _podman
+  _comps[podman]=_podman
+fi
+
+podman completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_podman" &|
+
 # alias to docker, run podman commands as docker
 if ! type "docker" > /dev/null; then
-  alias docker=podman
-  alias docker-compose=podman-compose
+  # alias docker=podman
+  # alias docker-compose=podman-compose
+
+  export DOCKER_HOST="unix://$(podman machine inspect | jq -r '.[0].ConnectionInfo.PodmanSocket.Path')"
 fi
 
 [[ -f "${0:h}/aliases" ]] && source "${0:h}/aliases"
