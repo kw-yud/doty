9c57ae1c43823a9d7a539786ac86ec950d07e8cf
Yudha Setiawan <yudha.setiawan@koinworks.com>
2024-03-25 16:43:59 +0700

    feat: add move line command

--- config/nvim/lua/doty/keymaps.lua
+++ config/nvim/lua/doty/keymaps.lua
@@ -1,14 +1,19 @@
 local funcs = require('doty.utils.functions')
 local nmap = funcs.nmap
+local vmap = funcs.vmap
 
 -- Remap command key
 nmap('<leader><leader>', ':')
 nmap('<C-p>', ':', {})
 
 -- window
-nmap('<leader>q', '<cmd>qall<cr>')
-nmap('<C-S>', '<cmd>w<cr>')
-nmap('<C-x>', '<cmd>x<cr>')
+-- This is going to get me cancelled
+nmap('<C-x>', '<cmd>x<CR>')
+nmap("<C-c>", "<cmd>q!<CR>")
+nmap('<C-s>', '<cmd>w<CR>')
+
+vmap("J", ":m '<-2<CR>gv=gv")
+vmap("K", ":m '>+2<CR>gv=gv")
 
 -- buffer navigation
 -- nmap('<leader>bp', ':bprev<cr>', { desc = 'Prev buffer' })
