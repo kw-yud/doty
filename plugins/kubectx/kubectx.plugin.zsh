0a4b79174dce6081e32fe63d401e77cdc65b4bad
Yudha Setiawan <yudha.setiawan@koinworks.com>
2024-08-12 11:12:57 +0700

    feat(k8s): add krew autocompletion

--- plugins/kubectx/kubectx.plugin.zsh
+++ plugins/kubectx/kubectx.plugin.zsh
@@ -6,3 +6,16 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 [ -f $HOME/.kube/fubectl.source ] && source $HOME/.kube/fubectl.source
 
 source "$ZSH/plugins/kubectx/kubectx.plugin.zsh"
+
+if type "kubectl-krew" > /dev/null; then
+  # If the completion file does not exist, generate it and then source it
+  # Otherwise, source it and regenerate in the background
+  if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl_krew" ]]; then
+    kubectl krew completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubectl_krew" >/dev/null
+  else
+    helm completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubectl_krew" >/dev/null &|
+  fi
+
+  source "$ZSH_CACHE_DIR/completions/_kubectl_krew"
+fi
+
