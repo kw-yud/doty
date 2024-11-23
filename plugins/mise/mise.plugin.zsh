144e74d33a3793eede7235120ac98f8c2be5bb48
Yudha Setiawan <yudha.setiawan@koinworks.com>
2024-11-23 16:15:27 +0700

    feat(doty): add mise plugin

---
+++ plugins/mise/mise.plugin.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://z-shell.github.io/zsh-plugin-assessor/Zsh-Plugin-Standard
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *b* ]] {
+  PATH="${0:h}/bin:${PATH}"
+}
+
+eval "$(mise activate zsh)"
+eval "$( mise activate zsh --shims)"
+
