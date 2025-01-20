76f7f792603d7d98f1d2d96d59b6242bb977af33
Yudha Setiawan <yudha.setiawan@koinworks.com>
2025-01-20 20:34:26 +0700

    fix(nvim): change lldb-dap abs path

--- config/nvim/lua/doty/plugins/nvim-dap.lua
+++ config/nvim/lua/doty/plugins/nvim-dap.lua
@@ -2,7 +2,7 @@ local dap = require("dap")
 
 dap.adapters.lldb = {
   type = "executable",
-  command = "/usr/bin/lldb-vscode-14", -- adjust as needed, must be absolute path
+  command = os.getenv("DOTY_DEVELOPMENT_DIRECTORY") .. "/llvm/bin/lldb-dap", -- adjust as needed, must be absolute path
   name = "lldb",
 }
 
